34bcba9dd625ee2ede92f0e0dedf247b27a7e4b0
Sri <sriratmurti.official@gmail.com>
2021-07-18 07:43:56 +0700

    Nambah home screen

--- Lynn/ViewController.swift
+++ Lynn/ViewController.swift
@@ -2,69 +2,23 @@
 //  ViewController.swift
 //  Lynn
 //
-//  Created by Sri Endah Ramurti on 17/07/21.
+//  Created by Sri Endah Ramurti on 18/07/21.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class homeScreen : UIViewController {
+    
     @IBOutlet weak var imageView: UIImageView!
     
-    private let scrollView = UIScrollView()
+    @IBOutlet weak var air: UIImageView!
     
-    var gambars = [UIImage(named: "satu"),UIImage(named: "tiga"),UIImage(named: "empat"),UIImage(named: "lima"),UIImage(named: "enam"),UIImage(named: "tujuh"),UIImage(named: "satu"),UIImage(named: "tiga")]
-    
-    private let pageControl: UIPageControl = {
-       let pageControl = UIPageControl()
-        pageControl.numberOfPages = 8
-        pageControl.backgroundColor = .clear
-        return pageControl
-    }()
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        scrollView.delegate = self
-        pageControl.addTarget(self, action: #selector(pagecontrolDidChange(_:)), for: .valueChanged)
-        scrollView.backgroundColor = .clear
-        view.addSubview(scrollView)
-        view.addSubview(pageControl)
+        super .viewDidLoad()
+        
+        imageView.image = UIImage(named: "satu")
+        air.image = UIImage(named: "Lynn")
     }
     
-    @objc private func pagecontrolDidChange(_ sender: UIPageControl){
-        let current = sender.currentPage
-        scrollView.setContentOffset(CGPoint(x: CGFloat(current) * view.frame.size.width, y: 0), animated: true)
-    }
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        pageControl.frame = CGRect(x: 20, y: view.frame.size.height-70, width: view.frame.size.width-20, height:80)
-        
-        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
-        
-        if scrollView.subviews.count == 2 {
-            configureScrollView()
-        }
-    }
-    
-    private func configureScrollView (){
-        scrollView.contentSize = CGSize(width: view.frame.size.width*5, height: scrollView.frame.size.height)
-        scrollView.isPagingEnabled = true
-        
-       
-        for x in 0 ..< 8 {
-            let page = UIImageView(frame: CGRect(x: CGFloat(x) * view.frame.size.width, y: 0, width: view.frame.size.width, height: scrollView.frame.size.height))
-          
-            
-            page.image = gambars[x]
-            scrollView.addSubview(page)
-        }
-    }
-    
-
-}
-
-extension ViewController : UIScrollViewDelegate{
-    func scrollViewDidScroll(_ scrollView: UIScrollView){
-        pageControl.currentPage = Int(Float(scrollView.contentOffset.x) / Float(scrollView.frame.size.width))
-    }
 }
